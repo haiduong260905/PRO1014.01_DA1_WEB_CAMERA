505066d104225c4e9e8b229449b2c89910214f69
ducnm <nguyenduc100xh@gmail.com>
2025-05-24 14:55:55 +0700

    Giao diện danh mục

---
+++ admin/danhmuc/list.php
@@ -0,0 +1,28 @@
+<div class="box-right">
+    <div class="title-page">
+        <p>Danh mục</p>
+    </div>
+
+    <div class="form-search">
+        <form action="" method="post">
+            <input type="text" name="kyw" placeholder="Nhập từ khóa">
+            <!-- Keyword -->
+            <input type="submit" name="timkiemdm" value="Tìm kiếm">
+        </form>
+    </div>
+
+    <div class="btn-form mr20">
+        <a href="index.php?act=adddm" class="btn-adddm">Thêm danh mục</a>
+    </div>
+
+    <div class="row form-content">
+        <table>
+            <tr>
+                <th>STT</th>
+                <th>Tên danh mục</th>
+                <th>Hình danh mục</th>
+                <th class="text-center">Thao tác</th>
+            </tr>
+        </table>
+    </div>
+</div>
