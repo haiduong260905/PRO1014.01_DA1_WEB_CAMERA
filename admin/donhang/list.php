aad96d2ab18b3c612fc7ef09469276150099aaaf
ducnm <nguyenduc100xh@gmail.com>
2025-05-25 22:46:56 +0700

    giao diện add danh mục, update

---
+++ admin/donhang/list.php
@@ -0,0 +1,16 @@
+<div class="box-right">
+    <div class="title-page">
+        <p>Đơn hàng</p>
+    </div>
+
+    <div class="form-search">
+        <form action="" method="post">
+            <input type="text" name="kyw" placeholder="Nhập từ khóa">
+            <input type="Submit" name="timkiemdh" value="Tìm kiếm">
+        </form>
+    </div>
+
+    <div class="row form-content">
+        <table></table>
+    </div>
+</div>
