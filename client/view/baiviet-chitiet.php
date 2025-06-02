afd174a8e1263a3c2661d382d67db2ec0f3117dd
haphong2004 <phonghmph33761@fpt.edu.vn>
2025-06-02 14:34:20 +0700

    baiviet,thongtin,lienhe,gioithieu

---
+++ client/view/baiviet-chitiet.php
@@ -0,0 +1,157 @@
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                <div class="page-breadcrumb">
+                    <ol class="breadcrumb">
+                        <li><a href="index.php">Trang chủ</a></li>
+                        <li>Bài Viết</li>
+                    </ol>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                        <div class="post-block ">
+                            <!-- post block -->
+                            <div class="post-content">
+                                <h2 class="post-title">[NEWS] CANON CẬP NHẬT FIRMWARE MỚI CHO EOS R5 C, EOS C70, EOS C300 Mark III, EOS C500 Mark II</h2>
+                                <div class="meta">
+                                    <span class="meta-date">11/11/2023</span>
+                                    <span>| &nbsp; &nbsp;</span>
+                                    <span class="meta-admin">By <a href="#" class="meta-title">Admin</a></span>
+                                 
+                                    
+                                </div>
+                                <div class="post-img">
+                                    <img src="https://www.newsshooter.com/wp-content/uploads/2022/09/Fc8YykOWIAINzcW.jpeg" alt=""class="img-responsive">
+                                </div>
+                                <p>Hôm nay, Canon đã công bố bản firmware mới nhất cho dòng  máy ảnh Cinema EOS, bao gồm: EOS C500 Mark II, EOS C300 Mark III, EOS C70, EOS R5 C, máy quay XF605, cũng như một số máy ảnh PTZ chọn lọc, mở rộng các lựa chọn sáng tạo và kỹ thuật có sẵn cho khách hàng của Canon. Ngoài ra, Canon đã công bố Ứng dụng điều khiển nhiều máy ảnh Canon mới (“ứng dụng”) 1 . Tất cả các bản cập nhật chương trình cơ sở và ứng dụng sẽ được cung cấp cho khách hàng tải xuống miễn phí vào đầu tháng 12 năm 2023*.
+
+                                    Cụ thể hơn, một số điểm chính bao gồm:
+                                    
+                                    EOS C500 Mark II / EOS C300 Mark III
+                                    
+                                    Thêm bộ chuyển đổi Tele kỹ thuật số.
+                                    Hỗ trợ điều khiển camera từ Bộ điều khiển camera từ xa RC-IP1000 mới.
+                                    Thêm tính linh hoạt với quy ước đặt tên tệp trên tệp proxy, giúp tăng cường khả năng tương thích với các hệ thống phần mềm chỉnh sửa phi tuyến tính.
+                                    Các cải tiến đối với phạm vi vectơ giờ đây sẽ cho phép hiển thị màu vectơ và không gian màu trên màn hình.
+                                    Thêm khả năng tương thích với Ống kính zoom linh hoạt CN-E31.5-95mm.
+                                    EOS C70
+                                    
+                                    Thêm bộ chuyển đổi Tele kỹ thuật số.
+                                    Hỗ trợ điều khiển camera từ Bộ điều khiển camera từ xa RC-IP1000 mới.
+                                    Thêm tính linh hoạt với quy ước đặt tên tệp trên tệp proxy, giúp tăng cường khả năng tương thích với các hệ thống phần mềm chỉnh sửa phi tuyến tính.
+                                    Bổ sung khả năng tương thích với ống kính RF24-105mm F2.8 L IS USM Z với Bộ điều hợp zoom điện và dòng ống kính rạp chiếu phim RF Prime.
+                                    Thêm khả năng tương thích với ống kính Super 35mm Flex Zoom của Canon.
+                                    EOS R5C
+                                    
+                                    Thêm tính linh hoạt với quy ước đặt tên tệp trên tệp proxy, giúp tăng cường khả năng tương thích với các hệ thống phần mềm chỉnh sửa phi tuyến tính.
+                                    Các cải tiến đối với phạm vi vectơ giờ đây sẽ cho phép hiển thị màu vectơ và không gian màu trên màn hình.
+                                    Thêm chức năng hiển thị khung hình trong khi ghi.
+                                    Thêm khả năng tương thích với ống kính RF24-105mm F2.8 L IS USM Z với Bộ điều hợp thu phóng điện và Ống kính điện ảnh RF Prime.
+                                    Thêm khả năng tương thích với ống kính Super 35mm Flex Zoom của Canon.
+                                    Thêm cải tiến cho chế độ chụp ảnh của máy ảnh.</p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                <div class="leave-comments">
+                    <div class="leave-comments-head">
+                        <h3 class="leave-comments-title">để lại bình luận</h3>
+                    </div>
+                    <div class="leave-comments-body">
+                        <div class="row">
+                            <form>
+                                <div class="col-lg-6 col-md-6 col-md-6 col-xs-12">
+                                    <div class="form-group">
+                                        <label class= "control-label required sr-only" for="name"></label>
+                                        <input id="name" name="name"type="text" class="form-control" placeholder="tên của bạn"required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-md-6 col-xs-12">
+                                    <div class="form-group">
+                                        <label class= "control-label required sr-only" for="name"></label>
+                                        <input id="email" name="email"type="email" class="form-control" placeholder="địa chỉ email"required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-md-6 col-xs-12">
+                                    <div class="form-group">
+                                        <label class= "control-label required sr-only" for="name"></label>
+                                        <textarea class="form-control" id="textarea" name="textarea" rows="5"
+                                                  placeholder="Bình luận"></textarea>
+                                    </div>
+                                </div>
+                                 <div class="col-lg-12 col-md-12 col-md-6 col-xs-12">
+                                    <div class="form-group">
+                                        <button id="singlebutton" name="singlebutton"
+                                                class="btn btn-primary btn-sm">Gửi
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <!-- widget-search-start -->
+                <div class="widget widget-search">
+                    <div class="widget-body">
+                        <form>
+                            <div class="search-form">
+                                <input type="text" class="form-control" placeholder="Tìm kiếm ở đây">
+                                <button type="Submit"><i class="fa fa-search"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>    
+                <div class="widget widget-categories">
+                    <div class="widget-head">
+                        <h2 class="widget-title">theo thể loại</h2>
+                    </div>
+                    <div class="widget-body">
+                        <ul class="arrow">
+                            <li class="active"><a href="#">ra mắt máy ảnh</a></li>
+                            <li><a href="#">cập nhật FIRMWARE</a></li>
+                            <li><a href="#">đánh giá sản phẩm</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="widget widget-categories">
+                    <div class="widget-head">
+                        <h2 class="widget-title">theo tháng</h2>
+                    </div>
+                    <div class="widget-body">
+                        <ul class="arrow">
+                            <li class="active"><a href="#">tháng 6(2025)</a></li>
+                            <li><a href="#">tháng 5</a></li>
+                            <li><a href="#">tháng 4</a></li>
+                            <li><a href="#">tháng 3</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="widget widget-categories">
+                    <div class="widget-head">
+                        <h2 class="widget-title">theo người dùng </h2>
+                    </div>
+                    <div class="widget-body">
+                        <ul class="arrow">
+                            <li class="active"><a href="#">admin</a></li>
+                            <li><a href="#">khách hàng</a></li>
+                            </ul>
+                            </div>
+                </div>
+                 </div>
+        </div>
+    </div>
+</div>
+                
---
+++ client/view/thongtin.php
@@ -0,0 +1,109 @@
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="page-breadcrumb">
+                    <ol class="breadcrumb">
+                        <li><a href="index.php">Trang chủ</a></li>
+                        <li>Thông tin</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="box">
+                    <div class="box-head">
+                        <h2 class="head-title">Chòa mừng bạn đến với 9-CAMERA</h2>
+                    </div>
+                    <div class="box-body">
+                        <p class="lead">9-CAMERA là một trang web bán máy ảnh trực tuyến</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="box">
+                    <div class="box-head">
+                        <h2 class="head-title">9-CAMERA là một công ty lớn</h2>
+                    </div>
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                <div class="couter-block">
+                                    <div class="couter-icon">
+                                        <img src="images/mobile_icon.png" alt="">
+                                    </div>
+                                    <div class="couter-content">
+                                        <h3 class="couter-text">100.000 <sup>+</sup></h3>
+                                        <h4>sản phẩm</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                <div class="couter-block">
+                                    <div class="couter-icon">
+                                        <img src="images/mobile_icon.png" alt="">
+                                    </div>
+                                    <div class="couter-content">
+                                        <h3 class="couter-text">10.000 <sup>+</sup></h3>
+                                        <h4>khách hàng</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                <div class="couter-block">
+                                    <div class="couter-icon">
+                                        <img src="images/mobile_icon.png" alt="">
+                                    </div>
+                                    <div class="couter-content">
+                                        <h3 class="couter-text">10 <sup>+</sup></h3>
+                                        <h4>chi nhánh</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mt40 mb40">
+            <div class="feature">
+                <div class="feature-content">
+                    <h3>Nhiện vụ</h3>
+                    <p>mang đến sự dễ dáng và thuận tiện cho khách hàng khi mua sản phẩm.</p>
+
+                </div>
+            </div>
+        </div>
+    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mt40 mb40">
+            <div class="feature">
+                <div class="feature-content">
+                    <h3>Tầm nhìn</h3>
+                    <p>mở rộng chuỗi cửa hàng ra cả nước. Phục vụ cho tất cả khách hàng.</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mt40 mb40">
+            <div class="feature">
+                <div class="feature-content">
+                    <h3>Giá cả</h3>
+                    <p>Giá cả phù hợp với từng sản phẩm. Ngoài ra chúng tôi còn có dịch vụ giảm giá và miễn phí vận chuyển.</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
