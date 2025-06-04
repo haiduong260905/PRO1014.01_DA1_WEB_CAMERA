ad578ba042701771cada2a2cf47fe140a90ab590
haiduong260905 <duongnhph48819@gmail.com>
2025-06-04 16:45:05 +0700

    test

--- model/mausac.php
+++ model/mausac.php
@@ -1,8 +1,13 @@
 <?php
+// function loadall_mausac(){
+//     $sql = "SELECT * FROM tb_mausac ORDER BY id DESC";
+//     // $listmausac = pdo_query($sql);
+//     // return $listmausac;
+//     return pdo_query($sql);
+// }
 function loadall_mausac(){
-    $sql = "SELECT * FROM tb_mausac ORDER BY id DESC";
-    // $listmausac = pdo_query($sql);
-    // return $listmausac;
-    return pdo_query($sql);
+    $sql = "SELECT * FROM tb_mausac";
+    $listms = pdo_query($sql);
+    return $listms;
 }
 ?>
